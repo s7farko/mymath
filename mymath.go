972ad36091b7077045ac355001b0ff325e011348
s7farko <160980651+s7farko@users.noreply.github.com>
2024-03-28 17:54:46 +0300

    Update mymath.go

--- mymath.go
+++ mymath.go
@@ -1,22 +1,53 @@
 package mymath
 
-import "math"
+import (
+	"math"
+)
 
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-func Ceil(x float64) float64 { return math.Ceil(x) }
+func Ceil(x float64) int {
+	intPart := int(x)
+	if x-float64(intPart) > 0 {
+		return intPart + 1
+	}
+	return intPart
+}
 
-func Floor(x float64) float64 {
-	return math.Floor(x)
+func Floor(x float64) int {
+	return int(x)
 }
-func Pow(x float64, y float64) float64 {
-	return math.Pow(x, y)
+
+func Pow(x, y float64) float64 {
+	var total float64 = 1
+	for i := 0; i < int(y); i++ {
+		total *= x
+	}
+	return total
 }
+
 func Max(x, y float64) float64 {
-	return math.Max(x, y)
+	if x > y {
+		return x
+	} else {
+		return y
+	}
 }
+
 func Min(x, y float64) float64 {
-	return math.Min(x, y)
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Yn(x int, y float64) float64 {
+	return math.Yn(x, y)
 }
